7109aaab9c4d98aa435ef24289661101d2c05ab0
Jessica Gladstone <jessgladtobe@hotmail.com>
2019-11-21 16:38:01 -0500

    change hungry blocks to use context, subject and described_class

--- dog_spec.rb
+++ dog_spec.rb
@@ -8,15 +8,22 @@ describe Dog do
             expect(subject.bark).to eql("Woof!") 
         end
     end
-
+# use context to set up conditions of subjects. Use described_class.new instead of instantiating a new specific object. 
+# this increases reusability of test code and easier refactoring.
     describe '#hungry?' do
-        it 'returns true if hunger greater than 5' do
-            hungry_dog = Dog.new(hunger: 7)
-            expect(hungry_dog).to be_hungry
+        context 'when hunger is greater than 5' do
+            subject{described_class.new(hunger: 7)}
+
+            it 'returns true ' do
+            expect(subject).to be_hungry
+            end
         end
-        it 'returns false if hunger is 5 or less' do
-            hungry_dog = Dog.new(hunger: 5)
-            expect(hungry_dog).to_not be_hungry
+        context 'when hunger is 5 or less' do
+            subject{described_class.new(hunger: 4)}
+
+            it 'returns false ' do
+            expect(subject).to_not be_hungry
+            end
         end
     end
 end
